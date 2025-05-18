cfd72ee49b5b67d4480d7996953c82c50bac4885
sasank <saisasank2323@gmail.com>
2025-05-18 16:49:29 +0530

    import export

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Hi from './hi.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
       <h1>hello</h1>
+      <Hi />
+
+      <Hi />
+
+      <Hi />
 
     </div>
   )
---
+++ src/hi.jsx
@@ -0,0 +1,11 @@
+
+
+export default function hi()
+{
+    return(
+        <div> 
+            <h1>hi</h1>
+            <p>hello world</p>
+        </div>
+    );
+}
